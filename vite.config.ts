6b476f698b857ac0e1b0622e115cb2b141855c2e
craigraphics <craigraphics@mail.com>
2024-08-06 11:49:32 -0700

    - update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/markdown-formatter/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+      },
+    },
+  },
 });
